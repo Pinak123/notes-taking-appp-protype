f4fbd96d6df1d8c52d7bf8ab1bb4e24359850134
pinak sandip sarode <pinaksarde1155@gmail.com>
2021-06-13 17:57:08 +0530

    first commit( note taking app)

---
+++ clases.js
@@ -0,0 +1,41 @@
+ let arr=[];
+ let shows;
+ let topic,compose;
+ function butt(x,y){
+    shows="";
+    topic =document.getElementById('topic').value;
+    compose = document.getElementById('composition').value;
+    class Note {
+        constructor(_topic,_compose) {
+            this.topic = _topic;
+            this.compose=_compose;
+        }
+    }
+    x= new Note(topic,compose);
+    arr.push(x);
+    if (topic=="") {
+        arr.pop();
+        //alert("Please enter topic of Note first!");
+    };
+    document.getElementById('topic').value="";
+    document.getElementById('composition').value="";
+    console.log(arr);
+    y=arr.map((a,b) =>{
+        return `<span class="note">Note #${b+1} </span><br> <span class="topic">Topic: ${a.topic}</span><br> <span class="compose">composition: ${a.compose}</span><br>
+        <button id="b${b}" onclick="arr.splice(${b},1);setTimeout(butt,0);setTimeout(show,0);">Deleate</button><br><br>
+        `
+    })
+    // document.getElementById('content').innerHTML=y;
+    shows=y;
+    if(shows=undefined){
+        shows='';
+    };
+}
+function show(){
+    if(shows=undefined){
+        shows='';
+    };
+    document.getElementById('content').innerHTML=shows;
+}
+
+
